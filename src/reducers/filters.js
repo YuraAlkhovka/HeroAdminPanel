7b9edacbefd9827ff56de4fa7699d160f4bb0ab1
Yura Alhovka <ualhovka@gmail.com>
2023-03-11 14:43:00 +0300

    filters.js

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const initialState = {
     filters: [],
     filtersLoadingStatus: 'idle',
@@ -32,39 +31,4 @@ const filters = (state = initialState, action) => {
     }
 }
 
-=======
-const initialState = {
-    filters: [],
-    filtersLoadingStatus: 'idle',
-    activeFilter: 'all'
-}
-
-const filters = (state = initialState, action) => {
-    switch (action.type) {
-        case 'FILTERS_FETCHING':
-            return {
-                ...state,
-                filtersLoadingStatus: 'loading'
-            }
-        case 'FILTERS_FETCHED':
-            return {
-                ...state,
-                filters: action.payload,
-                filtersLoadingStatus: 'idle'
-            }
-        case 'FILTERS_FETCHING_ERROR':
-            return {
-                ...state,
-                filtersLoadingStatus: 'error'
-            }
-        case 'ACTIVE_FILTER_CHANGED':
-            return {
-                ...state,
-                activeFilter: action.payload
-            }
-        default: return state
-    }
-}
-
->>>>>>> f4c74d1ebfbc06031a55cb2c31964436af71e346
 export default filters;
