2ec3429620224d0f24645526294953e6b0bff8f3
Yura Alhovka <ualhovka@gmail.com>
2023-03-11 21:33:35 +0300

    redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+import ReduxThunk from 'redux-thunk';
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
@@ -30,7 +31,7 @@ const store = createStore(
     combineReducers({heroes, filters}),
     compose(
         enhancer,
-        compose(applyMiddleware(stringMiddleware), 
+        compose(applyMiddleware(ReduxThunk, stringMiddleware), 
                 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
     )
 )
